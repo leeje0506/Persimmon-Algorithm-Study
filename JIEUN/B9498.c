56021da0871c3b708dc8a22a37e351456e87c0f3
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-07-19 00:50:27 +0900

    [BOJ] 시험 성적 / 브론즈 5

---
+++ JIEUN/B9498.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    
+    // 시험 점수를 입력받음
+    scanf("%d", &score);
+    
+    // 성적을 계산하여 출력
+    if (score >= 90 && score <= 100) {
+        printf("A\n");
+    } else if (score >= 80 && score <= 89) {
+        printf("B\n");
+    } else if (score >= 70 && score <= 79) {
+        printf("C\n");
+    } else if (score >= 60 && score <= 69) {
+        printf("D\n");
+    } else {
+        printf("F\n");
+    }
+    
+    return 0;
+}
